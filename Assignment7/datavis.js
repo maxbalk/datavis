d5219ffc8dff2036c3a2b797c6cd1ce3b6801b42
maxbalk <maximumbalk@gmail.com>
2019-10-17 12:58:23 -0500

    percent commits

--- Assignment7/datavis.js
+++ Assignment7/datavis.js
@@ -1,14 +1,33 @@
 const groupsUrl = "http://augur.osshealth.io:5000/api/unstable/repo-groups";
 
-
 async function getAllData(){
     let groups = await fetchData(groupsUrl);
     /*for(const group of groups){
-        console.log(group.repo_group_id);
+        await getTotalCommits(group.repo_group_id);
     }*/
-    let reposUrl = groupsUrl + "/" + groups[0].repo_group_id + "/repos" //do this for each repo group i guess
+    let total = await getTotalCommits(groups[0].repo_group_id);
+    //let top = await getTopCommitters(groups[0].repo_group_id, total);
+}
+
+async function getTopCommitters(groupID, total){
+    let topUrl = groupsUrl + "/" + groupID + "/top-committers?threshold=1";
+    let topComitters = await fetchData(topUrl);
+    console.dir(topComitters);
+    for(let committer of topComitters){
+        let proportion = committer.commits / total;
+        console.dir(proportion, committer.email);
+    }
+}
+
+async function getTotalCommits(groupID){
+    let total = 0;
+    let reposUrl = groupsUrl + "/" + groupID + "/repos";
     let repos = await fetchData(reposUrl);
-    console.dir(repos);
+    for(let repo of repos){
+        total += repo.commits_all_time;
+    }
+    console.dir(total);
+    getTopCommitters(groupID, total);
 }
 
 async function fetchData(url){
